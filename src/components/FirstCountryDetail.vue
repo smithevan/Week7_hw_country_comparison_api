7860a4e4e2a9eddbb01d966620ce5e7256c22da0
Evan Smith <smith.m.evan@gmail.com>
2020-01-19 21:35:00 +0000

    country details working with nmore details

--- src/components/FirstCountryDetail.vue
+++ src/components/FirstCountryDetail.vue
@@ -3,6 +3,10 @@
   <div v-if="firstCountry" id="countryDetail">
     <h2>{{firstCountry.name}}</h2>
     <p><span>Region</span>: {{firstCountry.region.value}}</p>
+    <p><span>Capital City</span>: {{firstCountry.capitalCity}}</p>
+    <p><span>Income Level</span>: {{firstCountry.incomeLevel.value}}</p>
+    <p><span>Longitude</span>: {{firstCountry.longitude}}</p>
+    <p><span>Latitude</span>: {{firstCountry.latitude}}</p>
   </div>
 
 </template>
--- src/components/SecondCountryDetail.vue
+++ src/components/SecondCountryDetail.vue
@@ -3,6 +3,10 @@
   <div v-if="secondCountry" id="countryDetail">
     <h2>{{secondCountry.name}}</h2>
     <p><span>Region</span>: {{secondCountry.region.value}}</p>
+    <p><span>Capital City</span>: {{secondCountry.capitalCity}}</p>
+    <p><span>Income Level</span>: {{secondCountry.incomeLevel.value}}</p>
+    <p><span>Longitude</span>: {{secondCountry.longitude}}</p>
+    <p><span>Latitude</span>: {{secondCountry.latitude}}</p>
   </div>
 
 </template>
