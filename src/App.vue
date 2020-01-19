1713bfbdbae6f4bdaef6c69ca8875fe70ae40a34
Evan Smith <smith.m.evan@gmail.com>
2020-01-19 17:34:47 +0000

    first country dro

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Compare Countries</h1>
+    <first-country :countries="countries"></first-country>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstCountry from './components/FirstCountry.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      countries: [{}], //array of country objects with VATs included
+      firstCountry: null, //Object of country that has been selected
+      secondCountry: null,
+      CompletedTrip: [] //object of the beer when it's been selected as favourite
+    }
+  },
+
+  mounted(){
+    fetch('http://api.worldbank.org/v2/country?format=json')
+      .then(response => response.json())
+      .then(countries => this.countries = countries[1])
+
+      eventBus.$on('first-selected-country', (country) => {
+      this.firstCountry = country
+  })
+},
+
+
   components: {
-    HelloWorld
+    'first-country': FirstCountry
   }
 }
 </script>
---
+++ src/components/FirstCountry.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+
+  <div>
+    <select v-on:change="handleSelect" v-model="firstCountry">
+        <option v-for="(country, index) in countries" :value="country" key="index">{{country.name}}</option>
+      </select>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'first-country',
+  props: ['countries'],
+  methods: {
+    handleSelect(){
+      eventBus$emit('first-country-selected', this.firstCountry)
+    }
+  }
+}
+
+</script>
+
+<style lang="css" scoped>
+</style>
